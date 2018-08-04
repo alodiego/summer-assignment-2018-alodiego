be35c936d9eb125e44ddad2e0095e0260774243d
alodiego <39908050+alodiego@users.noreply.github.com>
2018-08-04 13:13:38 -0400

    Delete FirstProg.java

--- FirstProg.java
+++
@@ -1,9 +0,0 @@
-public class FirstProg {
-
-    public static void main(String[] args) {
-
-        System.out.println("I, Diego Alonso, am determined to learn how to code.") ;
-
-        System.out.println("Today's date is, August 4, 2018.");
-    }
-}
