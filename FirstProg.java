4aee6c15b0f218d29f7808165d3d59cb4a6374d2
alodiego <39908050+alodiego@users.noreply.github.com>
2018-08-04 12:58:36 -0400

    Add files via upload

---
+++ FirstProg.java
@@ -0,0 +1,9 @@
+public class FirstProg {
+
+    public static void main(String[] args) {
+
+        System.out.println("I, Diego Alonso, am determined to learn how to code.") ;
+
+        System.out.println("Today's date is, August 4, 2018.");
+    }
+}
