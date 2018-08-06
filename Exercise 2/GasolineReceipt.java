4c09270a4a08553d5cad39e03286e8b8da939ba7
alodiego <39908050+alodiego@users.noreply.github.com>
2018-08-06 14:15:15 -0400

    Add files via upload

--- Exercise 2/GasolineReceipt.java
+++ Exercise 2/GasolineReceipt.java
@@ -6,41 +6,24 @@ public class GasolineReceipt {
 
         System.out.println( "|                        |" );
 
-        System.out.println( "|      CORNER STORE      |" );
+        System.out.println( "|         COSCO          |" );
 
         System.out.println( "|                        |" );
 
-        System.out.println( "| 2015-03-29  04:38PM    |" );
+        System.out.println( "| 2018-07-01  02:13PM    |" );
 
         System.out.println( "|                        |" );
 
-        System.out.println( "| Gallons:       10.870  |" );
+        System.out.println( "| Gallons:       10.002  |" );
 
         System.out.println( "| Price/gallon: $ 2.089  |" );
 
         System.out.println( "|                        |" );
 
-        System.out.println( "| Fuel total:  $ 22.71   |" );
+        System.out.println( "| Fuel total:  $ 29.56   |" );
 
         System.out.println( "|                        |" );
 
         System.out.println( "+------------------------+" );
     }
 }
-
-The Powershell Output
-
-PS C:\Users\tatom\Documents> javac .\GasolineReceipt.java
-PS C:\Users\tatom\Documents> java GasolineReceipt
-+------------------------+
-|                        |
-|      CORNER STORE      |
-|                        |
-| 2015-03-29  04:38PM    |
-|                        |
-| Gallons:       10.870  |
-| Price/gallon: $ 2.089  |
-|                        |
-| Fuel total:  $ 22.71   |
-|                        |
-+------------------------+
